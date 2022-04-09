18d2d204217567971b047c4a41bee56edd981567
Giordano <giordanosi2011@gmail.com>
2022-04-09 02:22:27 -0300

    (fix): refact components

--- src/App.js
+++ src/App.js
@@ -1,63 +1,23 @@
 import { useState } from "react";
 import "./App.css";
 import InputSearch from "./Components/InputSearch";
+import Tasks from "./Components/Task";
+import TodoList from "./Components/TodoList";
 
 function App() {
-  let [todoList, setTodoList] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  let [newItem, setNewItem] = useState("");
-
-
-  function addNewItem () {
-    if (newItem.length <= 0) {
-      alert("Please, add a new task");
-      return;
-    }
-
-    let itemIndex = todoList.indexOf(newItem);
-    if (itemIndex >= 0) {
-      alert("Repeated task");
-      return;
-    }
-
-    setTodoList([...todoList, newItem]);
-    setNewItem("");
-  }
-
-  function delItem(index) {
-    let tmpArray = [...todoList];
-    tmpArray.splice(index, 1);
-
-    setTodoList(tmpArray);
-  }
-
-  const filteredList = todoList.filter((item) =>
-    item.toLowerCase().includes(searchTerm.toLocaleLowerCase())
-  );
+  const [todoList, setTodoList] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
 
   return (
     <div className="container">
       <h1 className="list"> ToDo List </h1>
-      <InputSearch setSearch={setSearchTerm} search={searchTerm} />
-      <div className="new-item">
-        <input
-          className="todo-form__input"
-          placeholder="Create a new task"
-          value={newItem}
-          onChange={(value) => setNewItem(value.target.value)}
-          type="text"
-        />
-        <button onClick={() => addNewItem ()}> Save </button>
-      </div>
-
-      <ul className="todo-list">
-        {filteredList.map((item, index) => (
-          <li key={index} className="todo-item">
-            {item}
-            <button onClick={() => delItem(index)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+      <InputSearch todoList={todoList} setFilteredList={setFilteredList} />
+      <Tasks todoList={todoList} setTodoList={setTodoList} />
+      <TodoList
+        filteredList={filteredList}
+        setTodoList={setTodoList}
+        todoList={todoList}
+      />
     </div>
   );
 }
--- src/Components/InputSearch/index.js
+++ src/Components/InputSearch/index.js
@@ -1,16 +1,36 @@
+import { useEffect } from "react";
 import { MdSearch } from "react-icons/md";
-import "./styles.css"; 
+import "./styles.css";
 
+export default function InputSearch({ todoList, setFilteredList }) {
+  useEffect(() => {
+    setFilteredList(todoList);
+  }, [todoList]);
 
+  function handleSearch(event) {
+    const { value } = event.target;
 
+    if (value.length <= 0) {
+      setFilteredList(todoList);
+      return;
+    }
 
-export default function InputSearch({  search, setSearch }) {
+    if (value.length > 0) {
+      setFilteredList(
+        todoList.filter((item) =>
+          item.toLowerCase().includes(value.toLowerCase())
+        )
+      );
+    } else {
+      setFilteredList([]);
+    }
+  }
+
   return (
     <div className="search">
       <MdSearch className="search-icons" size="1.3em" />
       <input
-        onChange={(event) => setSearch(event.target.value)}
-        value={search}
+        onChange={(event) => handleSearch(event)}
         type="text"
         placeholder="Search your task..."
       />
